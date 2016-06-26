ffb5ab32dd9392b68354536571d153fb058bfc19
GoshaEgoryan <gosha.egorian@gmail.com>
2016-06-27 01:50:57 +0300

    Старт

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Гоша Егорян
+ *
+ * Created on 27 июня 2016 г., 1:48
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
